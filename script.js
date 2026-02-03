9ef5cdb46b766bb805656283e29834bcb4aef9ec
azsk4 <55152716+azsk4@users.noreply.github.com>
2026-02-03 01:27:25 +0000

    Update script.js

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ noBtn.addEventListener("mouseover", () => {
 // YES is clicked
 
 yesBtn.addEventListener("click", () => {
-    title.textContent = "Yayayayayyayayayayaya!";
+    title.textContent = "Yayayayayyayayayayayayayayayayayayayayayayaya!";
 
     catImg.src = "cat_dance.gif";
 
